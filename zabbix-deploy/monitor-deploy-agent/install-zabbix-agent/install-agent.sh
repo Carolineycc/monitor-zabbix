440081cf7b37c0637cbc329a4ccff4a37372e3db
Carolineycc <chengyuan20160630@163.com>
2017-08-24 05:02:03 -0400

    fix ceph install bug

--- zabbix-deploy/monitor-deploy-agent/install-zabbix-agent/install-agent.sh
+++ zabbix-deploy/monitor-deploy-agent/install-zabbix-agent/install-agent.sh
@@ -70,55 +70,56 @@ sed -i '294 i  UserParameter=openstack.serviceexist[*],/etc/zabbix/scripts/servi
 if [ $METADATA = controller ];then
     cp  /home/admin-openrc  /etc/zabbix/scripts
     cp ./script/controller/check-process-status-openstack.sh  /etc/zabbix/scripts
-	cp ./script/controller/thread.sh  /etc/zabbix/scripts                                    cp ./script/controller/top.sh  /etc/zabbix/scripts                                   
-	cp ./script/controller/processexist.sh  /etc/zabbix/scripts             
-	cp ./script/controller/check-dbsql.sh  /etc/zabbix/scripts
+    cp ./script/controller/thread.sh  /etc/zabbix/scripts                                    
+    cp ./script/controller/top.sh  /etc/zabbix/scripts                                   
+    cp ./script/controller/processexist.sh  /etc/zabbix/scripts             
+    cp ./script/controller/check-dbsql.sh  /etc/zabbix/scripts
     sed -i '295 i UserParameter=check-process-status-openstack[*],/etc/zabbix/scripts/check-process-status-openstack.sh $1 ' /etc/zabbix/zabbix_agentd.conf
     sed -i '296 i UserParameter=system.cpu.highload[*], /etc/zabbix/scripts/thread.sh $1 '  /etc/zabbix/zabbix_agentd.conf
     sed -i '297 i  UserParameter=system.cpu.top, /etc/zabbix/scripts/top.sh '  /etc/zabbix/zabbix_agentd.conf
     sed -i '298 i  UserParameter=system.cpu.processexist[*], /etc/zabbix/scripts/processexist.sh $1 $2 $3 ' /etc/zabbix/zabbix_agentd.conf
-	sed -i '299 i  UserParameter=FromDual.MySQL.check,/usr/local/fpmmm/bin/fpmmm --config=/etc/fpmmm/fpmmm.conf ' /etc/zabbix/zabbix_agentd.conf
-	sed -i '300 i  UserParameter=check-dbsql,/etc/zabbix/scripts/check-dbsql.sh '  /etc/zabbix/zabbix_agentd.conf
+    sed -i '299 i  UserParameter=FromDual.MySQL.check,/usr/local/fpmmm/bin/fpmmm --config=/etc/fpmmm/fpmmm.conf ' /etc/zabbix/zabbix_agentd.conf
+    sed -i '300 i  UserParameter=check-dbsql,/etc/zabbix/scripts/check-dbsql.sh '  /etc/zabbix/zabbix_agentd.conf
 else 
     continue 
 fi 
 
 #--------------For openstack compute item ---------
 if [ $METADATA = compute ];then
-	
-	cp ./script/compute/thread.sh  /etc/zabbix/scripts
-	cp ./script/compute/top.sh  /etc/zabbix/scripts
-	cp ./script/compute/processexist.sh  /etc/zabbix/scripts
-	cp ./script/compute/memory.sh  /etc/zabbix/scripts
-	sed -i '295 i  UserParameter=system.cpu.highload[*], /etc/zabbix/scripts/thread.sh $1 ' /etc/zabbix/zabbix_agentd.conf
-        sed -i '296 i  UserParameter=system.cpu.top, /etc/zabbix/scripts/top.sh '  /etc/zabbix/zabbix_agentd.conf
-	sed -i '297 i  UserParameter=system.cpu.processexist[*], /etc/zabbix/scripts/processexist.sh $1 $2 $3 ' /etc/zabbix/zabbix_agentd.conf
-	sed -i '298 i  UserParameter=vm_monitor[*], /etc/zabbix/scripts/processexist.sh $1 $2 $3 ' /etc/zabbix/zabbix_agentd.conf
-	sed -i '299 i  UserParameter=check-single-process-memory[*], /etc/zabbix/scripts/memory.sh $1 ' /etc/zabbix/zabbix_agentd.conf
+    cp ./script/compute/thread.sh  /etc/zabbix/scripts
+    cp ./script/compute/top.sh  /etc/zabbix/scripts
+    cp ./script/compute/processexist.sh  /etc/zabbix/scripts
+    cp ./script/compute/memory.sh  /etc/zabbix/scripts
+    sed -i '295 i  UserParameter=system.cpu.highload[*], /etc/zabbix/scripts/thread.sh $1 ' /etc/zabbix/zabbix_agentd.conf
+    sed -i '296 i  UserParameter=system.cpu.top, /etc/zabbix/scripts/top.sh '  /etc/zabbix/zabbix_agentd.conf
+    sed -i '297 i  UserParameter=system.cpu.processexist[*], /etc/zabbix/scripts/processexist.sh $1 $2 $3 ' /etc/zabbix/zabbix_agentd.conf
+    sed -i '298 i  UserParameter=vm_monitor[*], /etc/zabbix/scripts/processexist.sh $1 $2 $3 ' /etc/zabbix/zabbix_agentd.conf
+    sed -i '299 i  UserParameter=check-single-process-memory[*], /etc/zabbix/scripts/memory.sh $1 ' /etc/zabbix/zabbix_agentd.conf
 else
-	continue
+    continue
 fi
 
 #--------------add ceph support -------------------------
 if [ $METADATA = ceph ];then
-	usermod -a -G ceph zabbix
-	sed -i '296 i UserParameter=system.process.exist[*], /etc/zabbix/scripts/processexist.sh $1 $2 $3' /etc/zabbix/zabbix_agentd.conf
+    usermod -a -G ceph zabbix
+    cp ./script/ceph/* /etc/zabbix/scripts
+    sed -i '296 i UserParameter=system.process.exist[*], /etc/zabbix/scripts/processexist.sh $1 $2 $3' /etc/zabbix/zabbix_agentd.conf
     sed -i '297 i UserParameter=ceph.agent.json[*], /etc/zabbix/scripts/ceph-agent-json.py $1'  /etc/zabbix/zabbix_agentd.conf
-	sed -i '298 i UserParameter=ceph.disk.scan, /etc/zabbix/scripts/ceph_disk_scan.sh '  /etc/zabbix/zabbix_agentd.conf
-	sed -i '299 i UserParameter=ceph.disk.rrqm_s[*], iostat -dyx  |grep "\b$1\b"|awk '{print $$2}' ' /etc/zabbix/zabbix_agentd.conf
-    sed -i '300 i UserParameter=ceph.disk.wrqm_s[*], iostat -dyx  |grep "\b$1\b"|awk '{print $$3}' ' /etc/zabbix/zabbix_agentd.conf
-	sed -i '301 i UserParameter=ceph.disk.r_s[*], iostat -dyx  |grep "\b$1\b"|awk '{print $$4}'  '/etc/zabbix/zabbix_agentd.conf
-	sed -i '302 i UserParameter=ceph.disk.w_s[*], iostat -dyx  |grep "\b$1\b"|awk '{print $$5}'  ' /etc/zabbix/zabbix_agentd.conf
-	sed -i '303 i UserParameter=ceph.disk.rkB_s[*], iostat -dyx  |grep "\b$1\b"|awk '{print $$6}'  '   /etc/zabbix/zabbix_agentd.conf
-	sed -i '304 i UserParameter=ceph.disk.wkB_s[*], iostat -dyx  |grep "\b$1\b"|awk '{print $$7}'  '  /etc/zabbix/zabbix_agentd.conf
-	sed -i '305 i UserParameter=ceph.disk.avgrq-sz[*], iostat -dyx  |grep "\b$1\b"|awk '{print $$8}'   '   /etc/zabbix/zabbix_agentd.conf
-	sed -i '306 i UserParameter=ceph.disk.avgqu-sz[*], iostat -dyx  |grep "\b$1\b"|awk '{print $$9}'  '  /etc/zabbix/zabbix_agentd.conf
-	sed -i '307 i UserParameter=ceph.disk.await[*], iostat -dyx  |grep "\b$1\b"|awk '{print $$10}'  '   /etc/zabbix/zabbix_agentd.conf
-	sed -i '308 i UserParameter=ceph.disk.r_await[*], iostat -dyx  |grep "\b$1\b"|awk '{print $$11}'  '/etc/zabbix/zabbix_agentd.conf
-	sed -i '309 i UserParameter=ceph.disk.w_await[*], iostat -dyx  |grep "\b$1\b"|awk '{print $$12}'  ' /etc/zabbix/zabbix_agentd.conf
-	sed -i '310 i UserParameter=ceph.disk.svctm[*], iostat -dyx  |grep "\b$1\b"|awk '{print $$13}'  '  /etc/zabbix/zabbix_agentd.conf
-	sed -i '311 i UserParameter=ceph.disk.util[*], iostat -dyx  |grep "\b$1\b"|awk '{print $$14}'  '  /etc/zabbix/zabbix_agentd.conf
-	sed -i '312 i UserParameter=ceph.agent.test, /etc/zabbix/scripts/test.sh '  /etc/zabbix/zabbix_agentd.conf
+    sed -i '298 i UserParameter=ceph.disk.scan, /etc/zabbix/scripts/ceph_disk_scan.sh '  /etc/zabbix/zabbix_agentd.conf
+    sed -i "299 i UserParameter=ceph.disk.rrqm_s[*], iostat -dyx  |grep "\b$1\b"|awk '{print $$2}' " /etc/zabbix/zabbix_agentd.conf
+    sed -i "300 i UserParameter=ceph.disk.wrqm_s[*], iostat -dyx  |grep "\b$1\b"|awk '{print $$3}' " /etc/zabbix/zabbix_agentd.conf
+    sed -i "301 i UserParameter=ceph.disk.r_s[*], iostat -dyx  |grep "\b$1\b"|awk '{print $$4}'  " /etc/zabbix/zabbix_agentd.conf
+    sed -i "302 i UserParameter=ceph.disk.w_s[*], iostat -dyx  |grep "\b$1\b"|awk '{print $$5}'  " /etc/zabbix/zabbix_agentd.conf
+    sed -i "303 i UserParameter=ceph.disk.rkB_s[*], iostat -dyx  |grep "\b$1\b"|awk '{print $$6}'  "   /etc/zabbix/zabbix_agentd.conf
+    sed -i "304 i UserParameter=ceph.disk.wkB_s[*], iostat -dyx  |grep "\b$1\b"|awk '{print $$7}'  "  /etc/zabbix/zabbix_agentd.conf
+    sed -i "305 i UserParameter=ceph.disk.avgrq-sz[*], iostat -dyx  |grep "\b$1\b"|awk '{print $$8}'  "   /etc/zabbix/zabbix_agentd.conf
+    sed -i "306 i UserParameter=ceph.disk.avgqu-sz[*], iostat -dyx  |grep "\b$1\b"|awk '{print $$9}' "  /etc/zabbix/zabbix_agentd.conf
+    sed -i "307 i UserParameter=ceph.disk.await[*], iostat -dyx  |grep "\b$1\b"|awk '{print $$10}'   "  /etc/zabbix/zabbix_agentd.conf
+    sed -i "308 i UserParameter=ceph.disk.r_await[*], iostat -dyx  |grep "\b$1\b"|awk '{print $$11}'  "/etc/zabbix/zabbix_agentd.conf
+    sed -i "309 i UserParameter=ceph.disk.w_await[*], iostat -dyx  |grep "\b$1\b"|awk '{print $$12}' " /etc/zabbix/zabbix_agentd.conf
+    sed -i "310 i UserParameter=ceph.disk.svctm[*], iostat -dyx  |grep "\b$1\b"|awk '{print $$13}' "  /etc/zabbix/zabbix_agentd.conf
+    sed -i "311 i UserParameter=ceph.disk.util[*], iostat -dyx  |grep "\b$1\b"|awk '{print $$14}'  "  /etc/zabbix/zabbix_agentd.conf
+    sed -i "312 i UserParameter=ceph.agent.test, /etc/zabbix/scripts/test.sh "  /etc/zabbix/zabbix_agentd.conf
 fi
 
 #--------------end install zabbix agent---------------------
@@ -126,7 +127,7 @@ chown -R zabbix:zabbix /etc/zabbix/scripts
 chmod 700 /etc/zabbix/scripts/*
 systemctl enable zabbix-agent 1>/dev/null 2>&1  
 systemctl start zabbix-agent &&
-    debug "$?" "Start zabbix-agent daemon failed,did you set the selinux off ? "
+    debug "$?" "Start zabbix-agent daemon failed "
 echo -e "\e[1;32m Zabbix agent has been install on $YELLOW $(hostname) $NO_COLOR  \e[0m "
 echo -e "\e[1;32m You can go to the zabbix server page to add $YELLOW $(hostname) $NO_COLOR  \e[0m "
 echo -e "\e[1;32m The metadata  is $YELLOW $METADATA $NO_COLOR   \e[0m "
@@ -134,15 +135,15 @@ echo -e "\e[1;32m The metadata  is $YELLOW $METADATA $NO_COLOR   \e[0m "
 
 #--------------------clean agent env ----------
 function clean(){
-      echo -e "\e[31m Begin clean zabbix agent installed env ...\e[0m "
-      yum erase zabbix-agent zabbix-sender -y  1>/dev/null 2>&1
-      rm -rf /etc/zabbix
-      echo -e "\e[32m Finshed clean env \e[0m"
-      }
+   echo -e "\e[31m Begin clean zabbix agent installed env ...\e[0m "
+   yum erase zabbix-agent zabbix-sender -y  1>/dev/null 2>&1
+   rm -rf /etc/zabbix
+   echo -e "\e[32m Finshed clean env \e[0m"
+}
 
 if [ $(rpm -qa | grep zabbix | wc -l) -ge 1 ];then
-    clean
-    install
+   clean
+   install
 else
-    install
+   install
 fi
